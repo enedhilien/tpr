21ef1ec7435da1906ee8d661e8ed913313c5e96a
Blazej Burkot <bburkot@vnode-05.dydaktyka.icsr.agh.edu.pl>
2015-03-14 19:06:37 +0100

    fix mpi hosts

---
+++ lab1/test2.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+COUNT=20
+BYTES="1 2 3 4 5 6 7 8 9 10 20 30 40 50 60 70 80 90 100 125 150 200 250 300 400 500 750 1000 1400 1800 2400 3000 3500 4000 4500 5000 6000 7000 8000 9000 10000 12500 15000 17500 20000 25000 40000 70000 100000 150000 200000 400000 600000 800000 1000000"
+
+touch async.out
+touch sync.out
+
+echo "Msg[B];Send[us];Recv[us]" > async.out
+echo "Msg[B];Send[us];Recv[us]" > sync.out
+
+if [ $1 = "c" ]; then
+	make
+	PROGRAM="./send_recv"
+else
+	PROGRAM="./zad1.py"
+fi
+
+for BYTE in $BYTES
+do
+	mpiexec -machinefile ../mpihosts2 -np 2 ${PROGRAM} async $BYTE $COUNT >> async.out
+	mpiexec -machinefile ../mpihosts2 -np 2 ${PROGRAM} sync $BYTE $COUNT >> sync.out
+done
+
