c4abfab0b0016501a4d6a6846a1859fc1dcb1c3c
Robert Skibinski <enedhilien@gmail.com>
2015-04-12 20:08:13 +0200

    run

--- pi/sequential/run.sh
+++ pi/sequential/run.sh
@@ -2,18 +2,21 @@
 module load libs/boost/1.52.0
 module load libs/openblas/0.2.6
 module load ruby/2.0.0-p247
+module add plgrid/tools/python
+module add plgrid/libs/python-numpy
+module add plgrid/tools/openmpi/1.6.5-gnu-4.9.2-ib
 
 pip install --user mpi4py
 
 NOW=$(date "+%F_%T")
 
 mkdir $NOW
-cp rpi.c ./$NOW
+cp tpr/pi/sequential/pi.py ./$NOW
 cd $NOW
 
-chmod u+x ./rpi.c
+chmod u+x ./pi.py
 
-mpiexec -np 1 ./rpi 1000 >> rpi.csv
+mpiexec -np 5 ./pi.py 1000 >> rpi.csv
 #mpiexec -np 1 ./pi 10000 >> pi.txt
 #mpiexec -np 1 ./pi 100000 >> pi.txt
 #mpiexec -np 1 ./pi 100000 >> pi.txt
