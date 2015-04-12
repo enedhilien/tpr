b88bbda618ec98e3afde250c5be7f7b7216f0790
Robert Skibinski <enedhilien@gmail.com>
2015-04-12 09:56:55 -0700

    run

---
+++ pi/sequential/run.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+module load libs/boost/1.52.0
+module load libs/openblas/0.2.6
+module load ruby/2.0.0-p247
+
+pip install --user mpi4py
+
+NOW=$(date "+%F_%T")
+
+mkdir $NOW
+cp rpi.c ./$NOW
+cd $NOW
+
+chmod u+x ./rpi.c
+
+mpiexec -np 1 ./rpi 1000 >> rpi.csv
+#mpiexec -np 1 ./pi 10000 >> pi.txt
+#mpiexec -np 1 ./pi 100000 >> pi.txt
+#mpiexec -np 1 ./pi 100000 >> pi.txt
