bb406fa62bba852886931393e335415d97538c6b
Robert Skibinski <enedhilien@gmail.com>
2015-05-02 01:36:29 -0700

    c run

---
+++ lab1/c/run.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+module load libs/boost/1.52.0
+module load libs/openblas/0.2.6
+module load ruby/2.0.0-p247
+
+NOW=$(date "+%F_%T")
+PROGRAM="./send_recv"
+
+mkdir $NOW
+cp tpr/lab1/c/$PROGRAM.c ./$NOW
+cd $NOW
+
+make
+
+SIZES="1 64 128 256 1024 2056 4096 1048576 2097152 10485760 104857600"
+TIMES="1 10 100 1000 10000 100000 1000000 10000000 100000000"
+
+SIZES="1 10 100"
+TIMES="1 10"
+for TIME in $TIMES
+do
+	echo $TIME >> "ticker"
+	for SIZE in $SIZES
+	do
+		mpiexec -np 2 $PROGRAM async $SIZE $TIME >> "async.out"
+	done
+	for SIZE in $SIZES
+	do
+		mpiexec -np 2 $PROGRAM sync $SIZE $TIME >> "sync.out"
+	done
+done
