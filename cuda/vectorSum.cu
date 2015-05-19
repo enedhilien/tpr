a05ac24d4b88c0806e96da404b11d2444b706e9a
Mateusz Pszczolka <plgmatips@zeus.cyfronet.pl>
2015-05-19 11:03:51 +0200

    run on cpu and gpu

--- cuda/vectorSum.cu
+++ cuda/vectorSum.cu
@@ -3,27 +3,56 @@
 #include <stdlib.h>
 #include "helper_timer.h"
 
-__global__ void add (int *a, int *b, int *c) {
+__global__ void add (int *a, int *b, int *c, int N) {
 	int tid = blockIdx.x * blockDim.x + threadIdx.x;
 	if(tid < N) {
 		c[tid] = a[tid]+b[tid];
 	}
 }
-int main( int arc, char **argv )  {
+void add_with_cpu(int *a, int *b, int *c, int N){
+	for (int i=0; i < N; i++){
+		c[i] = a[i] + b[i];
+	}
+}
+
+float count ( int , int, int * );
+
+int main( int argc, char **argv )  {
 	if (argc < 2){
 		printf("usage: %s <vector_size>", argv[0]);
 		exit(-1);
 	}
 	int N = atoi(argv[1]);
+	/*if (!strcmp(argv[2], "gpu"))
+		use_gpu = 1;
+	else if (strcmp(argv[2], "cpu")){
+                printf("usage: %s <vector_size> <mode=[gpu|cpu]>\t\t (mode %s is incorrect)", argv[0], argv[2]);
+		exit(-1);
+	}*/
+        int *c = (int *) malloc(N * sizeof(int));
+        int *ccpu = (int *) malloc(N * sizeof(int));
 
-	int *a = malloc(N * sizeof(int));
-	int *b = malloc(N * sizeof(int));
-	int *c = malloc(N * sizeof(int));
+	float time = count(N, 1, c);
+	printf ("Time for the gpu: %f ms\n", time);
+
+	time = count(N, 1, ccpu);
+	printf ("Time for the cpu: %f ms\n", time);
+
+}
+float count ( int N, int use_gpu, int * c){
+
+
+	int *a = (int *) malloc(N * sizeof(int));
+	int *b = (int *) malloc(N * sizeof(int));
+//	int *c = (int *) malloc(N * sizeof(int));
 	int *dev_a, *dev_b, *dev_c;
 
-	cudaMalloc((void**)&dev_a,N * sizeof(int));
-	cudaMalloc((void**)&dev_b,N * sizeof(int));
-	cudaMalloc((void**)&dev_c,N * sizeof(int));
+	if (use_gpu){
+		cudaMalloc((void**)&dev_a,N * sizeof(int));
+		cudaMalloc((void**)&dev_b,N * sizeof(int));
+		cudaMalloc((void**)&dev_c,N * sizeof(int));
+	}
+
 	for (int i=0; i<N; i++) {
 		a[i] = i;
 		b[i] = i*2;
@@ -36,21 +65,32 @@ int main( int arc, char **argv )  {
 	sdkCreateTimer(&timer);
 	sdkResetTimer(&timer);
 	sdkStartTimer(&timer);
-
-	add <<<1,N>>> (dev_a,dev_b,dev_c);
+	
+	if (use_gpu)
+		add <<<1,N>>> (dev_a,dev_b,dev_c, N);
+	else 
+		add_with_cpu (a, b, c, N);
 
 	cudaThreadSynchronize();
 	sdkStopTimer(&timer);
 	float time = sdkGetTimerValue(&timer);
 	sdkDeleteTimer(&timer);
-	printf ("Time for the kernel: %f ms\n", time);	
+//	printf ("Time for the kernel: %f ms\n", time);	
 
-	cudaMemcpy(c, dev_c, N*sizeof(int), cudaMemcpyDeviceToHost);
+	if(use_gpu)
+		cudaMemcpy(c, dev_c, N*sizeof(int), cudaMemcpyDeviceToHost);
+	
 	for (int i = 0; i < N; i++) {
 		printf("%d+%d=%d\n", a[i], b[i], c[i]);
 	}
-	cudaFree(dev_a);
-	cudaFree(dev_b);
-	cudaFree(dev_c);
-	return 0;
+
+	if (use_gpu){
+		cudaFree(dev_a);
+		cudaFree(dev_b);
+		cudaFree(dev_c);
+	}
+	free(a);
+	free(b);
+
+	return time;
 }
